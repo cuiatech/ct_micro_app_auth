80339113d25c7f878baa7043bb628a641a0ad7ff
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-04-04 18:45:38 -0300

    update

--- lib/app/presentation/register/register_controller.dart
+++ lib/app/presentation/register/register_controller.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, use_build_context_synchronously
 
 import 'package:ct_micro_app_auth/app/auth_routers.dart';
 import 'package:ct_micro_commons_dependencies/ct_micro_commons_dependencies.dart';
